9480ca20462d03cb12058565bb735161ba040dbb
LINPINYAN <24019715@myrp.edu.sg>
2026-01-15 12:01:14 +0800

    L18 stuff please add more

--- App.js
+++ App.js
@@ -1,20 +1,60 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React,{useState, useEffect} from 'react';
+import { FlatList, StatusBar, Text, TextInput, View, Image} from 'react-native';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+//Exercise 1C - create originalData
+let originalData = [];
+
+const App = () => {
+    const [myData, setMyData] = useState([]);
+
+
+
+    //Add useEffect = Exercise 1B
+    useEffect(() => {
+        //Add fetch() = Exercise 1A
+        const myurl = "https://r1999charawebservice.onrender.com/allcharacters"
+        fetch (myurl)
+            .then((response) => {
+                return response.json();
+            })
+            .then((myJson) => {
+                setMyData(myJson);
+                originalData = myJson;
+            })
+    },[]);
+
+    //Added filterData - Exercise 1C
+    const FilterData = (text) => {
+        if(text!=''){
+            let myFilteredData = originalData.filter((item) =>
+                item.chara_name.toLowerCase().includes(text.toLowerCase()));
+            setMyData(myFilteredData);
+        }
+        else {
+            setMyData(originalData);
+        }
+    }
+
+
+    const renderItem = ({item, index}) => {
+        return (
+            <View style={{flexDirection:'row',justifyContent:'space-between',alignItems:'center', borderWidth:1, backgroundColor:"powderblue"}} >
+                <Text style={{ backgroundColor:'powderblue', color:'navy'}}>{item.chara_name}</Text>
+                <Image source ={{uri: item.chara_pic}}
+                       style={{ width: 200, height: 270 , justifyContent: "center" }} />
+            </View>
+        );
+    };
+
+    return (
+        <View>
+            <StatusBar/>
+            <Text>Search:</Text>
+            <TextInput style={{borderWidth:1}} onChangeText={(text)=>{FilterData(text)}}/>
+            <FlatList data={myData} renderItem={renderItem} />
+        </View>
+    );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+export default App;
+//yay
